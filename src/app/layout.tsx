a20898e03c0dd74791b18cfcb1f4dc6aaf59b996
Zack Piper <zpipe07@gmail.com>
2024-03-20 22:05:17 -0600

    set up  mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
 
-import "./globals.css";
+import theme from "../theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +20,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
-      </body>
-    </html>
+    <>
+      <CssBaseline />
+
+      <html lang="en">
+        <body className={inter.className}>
+          <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>
+              <Container maxWidth="sm">{children}</Container>
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    </>
   );
 }
