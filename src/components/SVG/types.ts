9784ba4e3af0d4e73eb3fa840e70bb6a21083e80
Zack Piper <zpipe07@gmail.com>
2024-04-09 10:34:05 -0600

    update import

--- src/components/SVG/types.ts
+++ src/components/SVG/types.ts
@@ -1,22 +1,23 @@
-import { Color, Decoration, DesignFormInputs, FontFamily } from "@/src/components/SignDesigner/SignDesignerForm";
+import { DesignFormInputs } from "@/src/components/SignDesigner"
+import { Color } from "@/src/components/SignDesigner/SignDesignerForm"
 
 export type SvgProps = {
-  height: number;
-  width: number;
-  borderWidth: number;
-  inputs: DesignFormInputs;
+  height: number
+  width: number
+  borderWidth: number
+  inputs: DesignFormInputs
 }
 
 export type PreviewSvgProps = {
-  height?: number;
-  width?: number;
+  height?: number
+  width?: number
 }
 
 export type FiligreeProps = {
-  height?: number;
-  width?: number;
-  x?: number;
-  y?: number;
-  transform?: string;
-  color?: Color;
-};
+  height?: number
+  width?: number
+  x?: number
+  y?: number
+  transform?: string
+  color?: Color
+}
