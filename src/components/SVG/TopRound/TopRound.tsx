b341c4a181d2990be8f6248246e8966b6a7db07a
Zack Piper <zpipe07@gmail.com>
2024-04-09 10:32:39 -0600

    delete comment

--- src/components/SVG/TopRound/TopRound.tsx
+++ src/components/SVG/TopRound/TopRound.tsx
@@ -24,8 +24,6 @@ export const TopRound: React.FC<SvgProps> = ({
 
   return (
     <svg
-      // width={width}
-      // height={height}
       viewBox={`0 0 ${width} ${height}`}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
