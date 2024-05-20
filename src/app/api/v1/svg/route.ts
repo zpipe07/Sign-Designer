b134ca4727b48336c902a24dc730cbebaf2240ea
Zack Piper <zpipe07@gmail.com>
2024-05-20 12:35:54 -0600

    clear svg endpoint

--- src/app/api/v1/svg/route.ts
+++ src/app/api/v1/svg/route.ts
@@ -1,42 +1,42 @@
-import React from "react"
-import opentype from "opentype.js"
-import { type NextRequest } from "next/server"
+// import React from "react"
+// import opentype from "opentype.js"
+// import { type NextRequest } from "next/server"
 
-import { SignDesignerVisualizerView } from "@/src/components/SignDesignerVisualizer"
-import { createProductOptionsMap } from "@/src/hooks/queries/useGetProduct"
-import { getBaseUrl } from "@/src/utils/vercel"
+// import { SignDesignerVisualizerView } from "@/src/components/SignDesignerVisualizer"
+// import { createProductOptionsMap } from "@/src/hooks/queries/useGetProduct"
+// import { getBaseUrl } from "@/src/utils/vercel"
 
-export async function GET(request: NextRequest) {
-  const res = await fetch(`${getBaseUrl()}/api/v1/products/112`)
-  const data = await res.json()
-  const productOptionsMap = createProductOptionsMap(data.product)
+// export async function GET(request: NextRequest) {
+//   const res = await fetch(`${getBaseUrl()}/api/v1/products/112`)
+//   const data = await res.json()
+//   const productOptionsMap = createProductOptionsMap(data.product)
 
-  const searchParams = request.nextUrl.searchParams
-  const shape = searchParams.get("shape")
-  const orientation = searchParams.get("orientation")
-  const size = searchParams.get("size")
-  const textLines = searchParams.get("textLines")
-  console.log({ shape, orientation, size })
+//   const searchParams = request.nextUrl.searchParams
+//   const shape = searchParams.get("shape")
+//   const orientation = searchParams.get("orientation")
+//   const size = searchParams.get("size")
+//   const textLines = searchParams.get("textLines")
+//   console.log({ shape, orientation, size })
 
-  const font = opentype.loadSync(
-    "public/fonts/AlbertSans-VariableFont_wght.ttf",
-  )
-  const ReactDOMServer = (await import("react-dom/server")).default
-  const component = React.createElement(SignDesignerVisualizerView, {
-    inputs: {
-      shape: "rectangle",
-      orientation: "horizontal",
-      size: "medium",
-      textLines: [{ value: "Hello world" }],
-      // color: { foregroundColor: "black", backgroundColor: "green" },
-      color: "black/green",
-      fontFamily: "times",
-      decoration: "",
-    },
-    font,
-    productOptionsMap,
-  })
-  const string = ReactDOMServer.renderToString(component)
+//   const font = opentype.loadSync(
+//     "public/fonts/AlbertSans-VariableFont_wght.ttf",
+//   )
+//   const ReactDOMServer = (await import("react-dom/server")).default
+//   const component = React.createElement(SignDesignerVisualizerView, {
+//     inputs: {
+//       shape: "rectangle",
+//       orientation: "horizontal",
+//       size: "medium",
+//       textLines: [{ value: "Hello world" }],
+//       // color: { foregroundColor: "black", backgroundColor: "green" },
+//       color: "black/green",
+//       fontFamily: "times",
+//       decoration: "",
+//     },
+//     font,
+//     productOptionsMap,
+//   })
+//   const string = ReactDOMServer.renderToString(component)
 
-  return Response.json({ string })
-}
+//   return Response.json({ string })
+// }
