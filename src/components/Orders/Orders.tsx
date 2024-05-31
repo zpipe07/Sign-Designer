bbe47da07c985d09a01dcd38f47b6faf4b1a01d2
Zack Piper <zpipe07@gmail.com>
2024-05-31 10:58:26 -0600

    create /orders and /products

---
+++ src/components/Orders/Orders.tsx
@@ -0,0 +1,9 @@
+import Typography from "@mui/material/Typography"
+
+export const Orders: React.FC = () => {
+  return (
+    <Typography variant="h3" component="h1">
+      Orders
+    </Typography>
+  )
+}
