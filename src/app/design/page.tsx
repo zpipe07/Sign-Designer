66b266bc023165cb97a81e939a2d92b679ac5003
Zack Piper <zpipe07@gmail.com>
2024-06-11 19:04:17 -0600

    initialize design form from query string

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -1,10 +1,19 @@
+"use client"
+
+import { useSearchParams } from "next/navigation"
+
 import { SignDesigner } from "@/src/components/SignDesigner"
 import { SignDetails } from "@/src/components/SignDetails"
+import { parseSearchParams } from "@/src/utils"
 
 export default function Page() {
+  const searchParams = useSearchParams()
+
+  const params = parseSearchParams(searchParams)
+
   return (
     <>
-      <SignDesigner />
+      <SignDesigner {...params} />
 
       <SignDetails />
     </>
--- src/components/SignDesigner/SignDesigner.tsx
+++ src/components/SignDesigner/SignDesigner.tsx
@@ -1,22 +1,45 @@
 "use client"
 
+import { FormProvider, useForm } from "react-hook-form"
 import { Card, useTheme } from "@mui/material"
 import Grid from "@mui/material/Grid"
 import Box from "@mui/material/Box"
 import CircularProgress from "@mui/material/CircularProgress"
 
 import { SignDesignerForm } from "@/src/components/SignDesigner/SignDesignerForm"
-import { SignDesignerVisualizer } from "@/src/components/SignDesignerVisualizer/SignDesignerVisualizer"
 import { useGetProduct } from "@/src/hooks/queries/useGetProduct"
-import { PriceDisplay } from "@/src/components/PriceDisplay"
 import { SignVisualizer } from "@/src/components/SignVisualizer"
+import { DesignFormInputs } from "@/src/components/SignDesigner/types"
 
 type Props = {
   isEditing?: boolean
-}
+} & Partial<DesignFormInputs>
 
-export const SignDesigner: React.FC<Props> = ({ isEditing }) => {
+export const SignDesigner: React.FC<Props> = ({
+  isEditing,
+  shape,
+  size,
+  color,
+  textLines,
+  fontFamily,
+  mountingStyle,
+}) => {
   const theme = useTheme()
+
+  const formMethods = useForm<DesignFormInputs>({
+    defaultValues: {
+      shape: shape || "rectangle",
+      size: size || "extra large",
+      color: color || "black/white",
+      fontFamily: fontFamily || "Albert",
+      mountingStyle: mountingStyle || "wall mounted",
+      textLines: textLines || [
+        { value: "" },
+        { value: "" },
+        { value: "" },
+      ],
+    },
+  })
 
   const { isLoading } = useGetProduct(112)
 
@@ -36,30 +59,32 @@ export const SignDesigner: React.FC<Props> = ({ isEditing }) => {
   }
 
   return (
-    <Box paddingTop={4} paddingBottom={4}>
-      <Grid container spacing={{ xs: 3, lg: 4 }}>
-        <Grid item xs={12} md={6}>
-          <Box
-            sx={{
-              [theme.breakpoints.up("md")]: {
-                position: "sticky",
-                top: theme.spacing(2),
-              },
-            }}
-          >
-            <Card elevation={3} sx={{ padding: 2 }}>
-              {/* <SignDesignerVisualizer /> */}
-              <SignVisualizer />
-            </Card>
+    <FormProvider {...formMethods}>
+      <Box paddingTop={4} paddingBottom={4}>
+        <Grid container spacing={{ xs: 3, lg: 4 }}>
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                [theme.breakpoints.up("md")]: {
+                  position: "sticky",
+                  top: theme.spacing(2),
+                },
+              }}
+            >
+              <Card elevation={3} sx={{ padding: 2 }}>
+                {/* <SignDesignerVisualizer /> */}
+                <SignVisualizer />
+              </Card>
 
-            {/* <PriceDisplay /> */}
-          </Box>
-        </Grid>
+              {/* <PriceDisplay /> */}
+            </Box>
+          </Grid>
 
-        <Grid item xs={12} md={6}>
-          <SignDesignerForm isEditing={isEditing} />
+          <Grid item xs={12} md={6}>
+            <SignDesignerForm isEditing={isEditing} />
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </FormProvider>
   )
 }
