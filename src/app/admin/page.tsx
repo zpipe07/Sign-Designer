4de87fd64fac6560e355552e8bb5856775f62f77
Zack Piper <zpipe07@gmail.com>
2024-06-21 11:27:33 -0600

    check for user on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,17 @@
+import { redirect } from "next/navigation"
 import Typography from "@mui/material/Typography"
 
+import { createClient } from "@/src/utils/supabase/server"
 import { Orders } from "@/src/components/Orders"
 
-export default function Page() {
+export default async function Page() {
+  const supabase = createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect("/log-in")
+  }
+
   return (
     <>
       <Typography variant="h3" component="h1">
