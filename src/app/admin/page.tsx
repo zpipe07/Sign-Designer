84873231cfe4844b33d7c1b71dad8872150af3ed
Zack Piper <zpipe07@gmail.com>
2024-07-22 11:12:03 -0600

    add container to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,15 +1,16 @@
 import Typography from "@mui/material/Typography"
+import { Container } from "@mui/material"
 
 import { Orders } from "@/src/components/Orders"
 
 export default function Page() {
   return (
-    <>
+    <Container>
       <Typography variant="h3" component="h1">
         Admin
       </Typography>
 
       <Orders />
-    </>
+    </Container>
   )
 }
