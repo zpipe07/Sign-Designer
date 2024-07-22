af86e2be9d032534a16874074790d157d1ee512a
Zack Piper <zpipe07@gmail.com>
2024-07-22 10:56:38 -0600

    add container to cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,15 +1,16 @@
 import Typography from "@mui/material/Typography"
+import { Container } from "@mui/material"
 
 import { Cart } from "@/src/components/Cart"
 
 export default function Page() {
   return (
-    <>
+    <Container>
       <Typography variant="h3" component="h1" marginTop={2}>
         Your cart
       </Typography>
 
       <Cart />
-    </>
+    </Container>
   )
 }
