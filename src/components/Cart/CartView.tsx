d3568e5419b55ad3fdb0ad90240036d54a22020c
Zack Piper <zpipe07@gmail.com>
2024-07-22 19:44:15 -0600

    delete comment

--- src/components/Cart/CartView.tsx
+++ src/components/Cart/CartView.tsx
@@ -1,6 +1,3 @@
-// import Image from "next/image"
-// import Link from "next/link"
-import Typography from "@mui/material/Typography"
 import Table from "@mui/material/Table"
 import TableHead from "@mui/material/TableHead"
 import TableContainer from "@mui/material/TableContainer"
@@ -8,8 +5,6 @@ import TableRow from "@mui/material/TableRow"
 import TableCell from "@mui/material/TableCell"
 import TableBody from "@mui/material/TableBody"
 import Box from "@mui/material/Box"
-// import Button from "@mui/material/Button"
-// import EditIcon from "@mui/icons-material/Edit"
 
 import { VercelCart } from "@/src/lib/bigcommerce/types"
 import { CheckoutButton } from "@/src/components/CheckoutButton"
@@ -47,16 +42,6 @@ export const CartView: React.FC<Props> = ({ cart }) => {
           </TableBody>
         </Table>
       </TableContainer>
-
-      {/* <Box marginTop={2} marginBottom={2} textAlign="right">
-        <Typography variant="h5">Subtotal</Typography>
-        <Typography>${cart.cost.subtotalAmount.amount}</Typography>
-        <Typography marginBottom={1} variant="body2">
-          Shipping & taxes calculated at checkout
-        </Typography>
-
-        <CheckoutButton />
-      </Box> */}
     </>
   )
 }
