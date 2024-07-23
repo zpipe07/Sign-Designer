91c453eb1c44b932b43511d8ae846bdbd2cbdb3b
Zack Piper <zpipe07@gmail.com>
2024-07-22 18:11:56 -0600

    use checkout endpoint

--- src/components/CheckoutButton/CheckoutButton.tsx
+++ src/components/CheckoutButton/CheckoutButton.tsx
@@ -2,15 +2,19 @@ import Typography from "@mui/material/Typography"
 import Button from "@mui/material/Button"
 
 import { useGetCart } from "@/src/hooks/queries/useGetCart"
+import { useGetCheckout } from "@/src/hooks/queries/useGetCheckout"
 
 export const CheckoutButton: React.FC = () => {
   const { data, isLoading } = useGetCart()
 
-  if (isLoading) {
+  const { data: checkoutData, isLoading: isLoadingCheckout } =
+    useGetCheckout(data?.cart?.id)
+
+  if (isLoading || isLoadingCheckout) {
     return null
   }
 
-  if (!data?.cart?.checkoutUrl) {
+  if (!checkoutData?.data?.checkout_url) {
     return (
       <Typography>
         There was an error generating the checkout URL
@@ -23,7 +27,7 @@ export const CheckoutButton: React.FC = () => {
       variant="contained"
       size="large"
       color="secondary"
-      href={data.cart.checkoutUrl}
+      href={checkoutData.data.checkout_url}
     >
       Checkout now
     </Button>
