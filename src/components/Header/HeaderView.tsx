c893bb87bf4000d3891e3b2f2e871671d1c708a5
Zack Piper <zpipe07@gmail.com>
2024-07-30 12:17:57 -0600

    update header nav

--- src/components/Header/HeaderView.tsx
+++ src/components/Header/HeaderView.tsx
@@ -6,27 +6,21 @@ import Image from "next/image"
 import Link from "next/link"
 import Box from "@mui/material/Box"
 import IconButton from "@mui/material/IconButton"
-import Menu from "@mui/material/Menu"
-import MenuItem from "@mui/material/MenuItem"
-import Typography from "@mui/material/Typography"
 import Button from "@mui/material/Button"
-import Tooltip from "@mui/material/Tooltip"
-import Avatar from "@mui/material/Avatar"
 import MenuIcon from "@mui/icons-material/Menu"
-import AppBar from "@mui/material/AppBar"
 import Container from "@mui/material/Container"
-import Toolbar from "@mui/material/Toolbar"
+import MuiLink from "@mui/material/Link"
 import { useTheme } from "@mui/material"
+import { grey } from "@mui/material/colors"
 
-import { SignOut } from "@/src/components/Header/SignOut"
 import { Cart } from "@/src/components/Header/Cart"
 
 const pages = [
-  { label: "Sign Designer", href: "/design" },
-  { label: "Our Work", href: "/our-work" },
-  // { label: "About Us", href: "/about-us" },
-  // { label: "Contact Us", href: "/contact-us" },
-  { label: "FAQs", href: "/faqs" },
+  { label: "Create Your Sign", href: "/design" },
+  { label: "View Our Work", href: "/our-work" },
+  { label: "About Us", href: "/about-us" },
+  { label: "Contact Us", href: "/contact-us" },
+  // { label: "FAQs", href: "/faqs" },
 ]
 
 type Props = {
@@ -34,198 +28,146 @@ type Props = {
 }
 
 export const HeaderView: React.FC<Props> = ({ user }) => {
-  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(
-    null,
-  )
-  const [anchorElUser, setAnchorElUser] =
-    useState<null | HTMLElement>(null)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  const handleOpenNavMenu = (
-    event: React.MouseEvent<HTMLElement>,
-  ) => {
-    setAnchorElNav(event.currentTarget)
-  }
-  const handleOpenUserMenu = (
-    event: React.MouseEvent<HTMLElement>,
-  ) => {
-    setAnchorElUser(event.currentTarget)
-  }
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null)
-  }
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null)
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev)
   }
 
   const theme = useTheme()
 
   return (
-    <AppBar
-      position="static"
-      sx={{
-        color: theme.palette.text.primary,
-        backgroundColor: theme.palette.background.default,
-        boxShadow: "0 4px 15px 6px rgba(165, 165, 165, 0.2)",
-      }}
-    >
-      <Container maxWidth="lg">
-        <Toolbar disableGutters>
-          <Typography
-            variant="body1"
-            noWrap
+    <>
+      <Box component="header" sx={{ py: 2 }}>
+        <Container sx={{ position: "relative", textAlign: "center" }}>
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: theme.spacing(1),
+              transform: "translateY(-50%)",
+
+              [theme.breakpoints.up("sm")]: {
+                display: "none",
+              },
+            }}
+          >
+            <IconButton onClick={toggleMenu}>
+              <MenuIcon />
+            </IconButton>
+          </Box>
+
+          <MuiLink
             component={Link}
             href="/"
             sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontWeight: 700,
-              fontSize: 26,
-              color: "inherit",
-              textDecoration: "none",
+              display: "inline-flex",
+              alignItems: "center",
+
+              [theme.breakpoints.up("md")]: {
+                flexDirection: "column-reverse",
+              },
             }}
           >
-            Sign Genie
-          </Typography>
-          <Box sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}>
+            <Image
+              src="/images/logos/LogoTextBlue.png"
+              alt="Sign Genie"
+              width={851 / 5}
+              height={188 / 5}
+              style={{
+                padding: 4,
+              }}
+            />
             <Image
               src="/images/logos/LogoMarkBlue.png"
               alt=""
               width={540 / 6}
               height={289 / 6}
+              style={{
+                padding: 4,
+              }}
             />
-          </Box>
+          </MuiLink>
 
           <Box
-            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-          >
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none", zIndex: 99999 },
-              }}
-            >
-              {pages.map(({ label, href }) => (
-                // @ts-ignore
-                <MenuItem
-                  key={label}
-                  component={Link}
-                  href={href}
-                  onClick={handleCloseNavMenu}
-                >
-                  <Typography textAlign="center">{label}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          <Box sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}>
-            <Image
-              src="/images/logos/LogoMarkBlue.png"
-              alt=""
-              width={540 / 7}
-              height={289 / 7}
-            />
-          </Box>
-          <Typography
-            variant="body1"
-            noWrap
-            component={Link}
-            href="/"
             sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontWeight: 700,
-              color: "inherit",
-              textDecoration: "none",
-              fontSize: 24,
+              position: "absolute",
+              top: "50%",
+              right: theme.spacing(1),
+              transform: "translateY(-50%)",
+
+              [theme.breakpoints.down("md")]: {
+                right: theme.spacing(2),
+              },
             }}
           >
-            Sign Genie
-          </Typography>
-          <Box
-            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
-          >
-            {pages.map(({ label, href }) => (
+            <Cart />
+          </Box>
+        </Container>
+      </Box>
+
+      <Box
+        component="nav"
+        sx={{
+          backgroundColor: grey[900],
+
+          [theme.breakpoints.down("sm")]: {
+            display: isMenuOpen ? "block" : "none",
+            position: "absolute",
+            left: 0,
+            right: 0,
+            zIndex: 9999,
+          },
+        }}
+      >
+        <Box
+          component="ul"
+          sx={{
+            margin: 0,
+            padding: 0,
+            listStyle: "none",
+
+            [theme.breakpoints.up("sm")]: {
+              display: "flex",
+              justifyContent: "center",
+            },
+          }}
+        >
+          {pages.map(({ label, href }) => (
+            <Box
+              component="li"
+              key={label}
+              sx={{
+                "&:not(:last-child)": {
+                  borderBottom: "1px solid rgba(255, 255, 255, 0.2)",
+
+                  [theme.breakpoints.up("sm")]: {
+                    borderBottom: "none",
+                    borderRight: "1px solid rgba(255, 255, 255, 0.2)",
+                  },
+                },
+              }}
+            >
               <Button
                 component={Link}
                 href={href}
-                key={label}
-                onClick={handleCloseNavMenu}
                 sx={{
-                  fontSize: 18,
-                  padding: 2,
-                  borderRadius: 0,
-                  color: "inherit",
                   display: "block",
-                  textTransform: "none",
+                  color: theme.palette.common.white,
+                  py: 2,
+                  px: 2,
+
+                  [theme.breakpoints.up("md")]: {
+                    px: 4,
+                  },
                 }}
               >
                 {label}
               </Button>
-            ))}
-          </Box>
-
-          <Cart />
-
-          <Box sx={{ flexGrow: 0 }}>
-            {user ? (
-              <>
-                <Tooltip title="Open settings">
-                  <IconButton
-                    onClick={handleOpenUserMenu}
-                    sx={{ p: 0 }}
-                  >
-                    <Avatar />
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: "45px" }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
-                >
-                  <SignOut />
-                </Menu>
-              </>
-            ) : null}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </>
   )
 }
